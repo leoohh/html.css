0231b538ec8a4afa92b880324f5edb57b8a69256
leoohh <144128024+leoohh@users.noreply.github.com>
2024-04-17 02:25:24 -0300

    Create relogio+.js

---
+++ relogio+.js
@@ -0,0 +1,82 @@
+//dom
+
+const cumprimento = document.querySelector ('#cumprimento')
+const ano = document.querySelector('#ano')
+const data = document.querySelector('#data')
+const mes = document.querySelector('#mes')
+const horas = document.querySelector('#horas')
+const minutos = document.querySelector('#minutos')
+const segundos = document.querySelector('#segundos')
+const escanor = document.querySelector('#escanor')
+//eventos
+
+setInterval(()=>{
+
+   let dia = new Date()
+   //let varia "const" não
+   let d = dia.getDate()
+   let mess = dia.getMonth()+1
+   let a = dia.getFullYear()   
+   let h = dia.getHours()
+   let m = dia.getMinutes()
+   let s = dia.getSeconds()
+        
+if(h<10){
+   h="0"+ h
+}
+
+if(m<10){
+   m="0"+ m 
+}
+if(s<10){
+   s="0"+ s
+}
+if(d<10){
+   d="0"+ d
+}
+if(mess<10){
+   mess="0"+ mess
+}
+
+
+
+if(h>=5 && h < 18){
+   
+  escanor.src = "images/scannor forte.jpg"
+
+}
+else{
+  escanor.src = "images/scanor fraco.jpg"
+}
+
+
+if(h == 12){
+
+escanor.src = "images/scanor the one.jpg"
+
+}
+
+if(h>= 5 && h < 12)
+{
+cumprimento.textContent = 'Bom dia!'
+   
+}
+else if(h>= 12 && h < 18)
+{
+cumprimento.textContent = 'Boa tarde!'
+   
+}
+else
+{
+
+cumprimento.textContent = 'Boa noite!'
+   
+}
+
+   data.textContent = d
+   ano.textContent = a
+   mes.textContent = mess
+   horas.textContent = h
+   minutos.textContent = m
+   segundos.textContent = s
+},1000)
